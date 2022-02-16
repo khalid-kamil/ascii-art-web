e87b4bf0d90079b3cd9952c5629aef2b292f53fd
Adriell <93883453+c0decplyre@users.noreply.github.com>
2022-02-16 21:33:47 +0000

    Add test file for the server

--- server_test.go
+++ server_test.go
@@ -2,23 +2,86 @@ package main
 
 import (
 	"net/http"
+	"net/http/httptest"
+	"net/url"
+	"strings"
 	"testing"
 )
 
-func Test_process(t *testing.T) {
-	type args struct {
-		w http.ResponseWriter
-		r *http.Request
-	}
-	tests := []struct {
-		name string
-		args args
+func TestAsciiArt(t *testing.T) {
+	testCases := []struct {
+		banner           string
+		input            string
+		expectedResponse int
 	}{
-		// TODO: Add test cases.
+		{
+			// when valid banner and input is passed
+			banner:           "shadow",
+			input:            "this is history",
+			expectedResponse: http.StatusOK,
+		},
+		{
+			// when an invalid input file is passed
+			banner:           "shadow",
+			input:            "",
+			expectedResponse: http.StatusBadRequest,
+		},
+		{
+			// when an invalid banner file
+			banner:           "shadwo",
+			input:            "this is history",
+			expectedResponse: http.StatusInternalServerError,
+		},
 	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			process(tt.args.w, tt.args.r)
-		})
+
+	for _, tc := range testCases {
+		form := url.Values{}
+		form.Add("banner", tc.banner)
+		form.Add("input-name", tc.input)
+
+		request := httptest.NewRequest("POST", "/ascii-art", strings.NewReader(form.Encode()))
+		request.PostForm = form
+		responseRecorder := httptest.NewRecorder()
+
+		Process(responseRecorder, request)
+		if responseRecorder.Code != tc.expectedResponse {
+			t.Errorf("Want status '%d', got '%d'", tc.expectedResponse, responseRecorder.Code)
+		}
+		// assert.Equal(t, responseRecorder.Code, tc.expectedResponse)
+	}
+}
+
+func TestPostEndpoint(t *testing.T) {
+	svr := httptest.NewServer(http.HandlerFunc(Process))
+	defer svr.Close()
+	form := url.Values{}
+	form.Add("banner", "thinkertoy")
+	form.Add("input-name", "hello sad world")
+
+	request, err := http.NewRequest("POST", svr.URL+"/ascii-art", strings.NewReader(form.Encode()))
+	if err != nil {
+		t.Fatal(err)
+	}
+	request.Header.Add("Content-Type", "application/x-www-form-urlencoded")
+
+	resp, err := http.DefaultClient.Do(request)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if resp.StatusCode != 200 {
+		t.Errorf("Want status '%d', got '%d'", 200, resp.StatusCode)
+	}
+}
+
+func TestGetEndpoint(t *testing.T) {
+	svr := httptest.NewServer(http.HandlerFunc(Process))
+	defer svr.Close()
+	resp, err := http.Get(svr.URL + "/")
+	if err != nil {
+		t.Fatal(err)
+	}
+	if resp.StatusCode != 200 {
+		t.Errorf("Want status '%d', got '%d'", 200, resp.StatusCode)
 	}
 }
